e7e73b5acc479c8c1e57da7af60e783822900129
Your Correct GitHub Username <your-email@example.com>
2025-11-22 11:49:48 +0200

    fixed navbar2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,11 +7,11 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="logo">PBS Electrical Solutions.</div>
       <div className="nav-links">
+        <Link to="/Home" className="nav-btn">Home</Link>
         <Link to="/ProductPage" className="nav-btn">Products</Link>
         <Link to="/Wishlist" className="nav-btn">Wishlist</Link>
         <Link to="/Cart" className="nav-btn">Cart</Link>
         <Link to="/profile" className="nav-btn">Profile</Link>
-        <Link to="/Home" className="nav-btn">Profile</Link>
       </div>
     </nav>
   );
